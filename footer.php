2d2eeb8836e423b2e8df991d0b087506c964679a
永田健作 <>
2020-10-11 22:23:48 +0900

    footerに固定ページへのリンクを追加

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <!-- footer -->
 <footer>
-    ショップ情報|ヒストリー
+<a href="<?php echo get_page_link('5');?>">ショップ情報</a>
+|
+<a href="<?php echo get_page_link('79');?>">ヒストリー</a>
     <p class="footer__copyright"><small>&copy; <?php bloginfo( 'name' ); ?> 2019</small></p>
 </footer>
 <?php wp_footer();?>
