a541f210ffea956115f7fd64c5514ed5b99b8672
永田健作 <>
2020-10-18 00:22:11 +0900

    ハンバーガーメニューの調整 メニュー展開時には背景を薄グレイに

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,6 +2,7 @@ jQuery( function( $ ) {
 	$( ".p-sidebar_button" ).on( "click", function() {
 		$( this ).toggleClass( "is-open" );
 		$( ".l-sidebar" ).toggleClass( "is-open" );
+		$( ".menu-bg" ).toggleClass( "is-open" );
 		$( "body" ).toggleClass( "is-open" );
 	} );
 } );
--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,6 @@
 <!-- サイドバー -->
 <section class="l-sidebar">
-<button class="p-sidebar_button">
-            <div class="p-sidebar_button_line">
-            <span class="p-sidebar_button_lines p-sidebar_button_line_1"></span>
-            <span class="p-sidebar_button_lines p-sidebar_button_line_2"></span>
-            <span class="p-sidebar_button_lines p-sidebar_button_line_3"></span>
-        </div>
-            <h2>Menu</h2></button>
+
         <h2 class="l-sidebar_title">Menu</h2>
     <ul class="l-sidebar_menu"> <!--参考サイトhttps://shu-sait.com/wordpress-custom-menu-class/-->
            <?php 
